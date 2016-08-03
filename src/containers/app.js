9cd94ba64e3637b2e228bf4ca4e87596448af595
Chris Brown <chrisjosbrown@gmail.com>
2016-08-03 13:49:30 +0200

    feat(auth): WD-5154 use material design for login/nav
    imported material-ui, implemented it for login and navigation
    
    undefined

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,15 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { loginUser, logoutUser } from 'base/actions/session';
 
-import { Link } from 'react-router';
-import Button from 'base/components/button';
 import Content from 'base/components/content';
 import LoginModal from 'base/components/login/login-modal';
-import Logo from 'base/components/logo';
-import Navigator from 'base/components/navigator';
-import NavigatorItem from 'base/components/navigator-item';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
+import AccessibilityIcon from 'material-ui/svg-icons/action/accessibility';
+import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
+import { List, ListItem } from 'material-ui/List';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 function mapStateToProps(state) {
   return {
@@ -25,6 +37,8 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
+const muiTheme = getMuiTheme();
+
 class App extends Component {
 
   static propTypes = {
@@ -34,59 +48,86 @@ class App extends Component {
     logout: PropTypes.func,
   };
 
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      drawerOpen: false,
+    };
   }
 
   render() {
     const { session, children, login, logout } = this.props;
+    const { drawerOpen } = this.state;
 
-    // const token = session.get('token', false);
-    const isLoggedIn = true; // token && token !== null && typeof token !== 'undefined';
-    const firstName = session.getIn(['user', 'first'], '');
-    const lastName = session.getIn(['user', 'last'], '');
+    const token = session.get('token', false);
+    const isLoggedIn = token && token !== null && typeof token !== 'undefined';
 
     return (
-      <div>
-        <LoginModal
-          onSubmit={ login }
-          isPending={ session.get('isLoading', false) }
-          hasError={ session.get('hasError', false) }
-          isVisible={ !isLoggedIn } />
-        <Navigator testid="navigator">
-          <NavigatorItem mr>
-            <Logo />
-          </NavigatorItem>
-          <NavigatorItem isVisible={ false } mr>
-            <Link to="/counter">Counter</Link>
-          </NavigatorItem>
-          <NavigatorItem isVisible={ false } mr>
-            <Link to="/about">About Us</Link>
-          </NavigatorItem>
-          <NavigatorItem isVisible={ isLoggedIn } mr>
-            <Link to="/">Pick List</Link>
-          </NavigatorItem>
-          <NavigatorItem isVisible={ false } mr>
-            <Link to="/settings">Settings</Link>
-          </NavigatorItem>
-          <NavigatorItem isVisible={ false }>
-            <Link to="/scan">Scan</Link>
-          </NavigatorItem>
-          <div className="flex flex-auto"></div>
-          <NavigatorItem isVisible={ false } mr>
-            <div data-testid="user-profile" className="h3">{ `${firstName} ${lastName}` }</div>
-          </NavigatorItem>
-          <NavigatorItem isVisible={ false }>
-            <Button disabled onClick={ logout } className="bg-red white">
-              Logout
-            </Button>
-          </NavigatorItem>
-        </Navigator>
-        <Content isVisible={ isLoggedIn }>
-          { children }
-        </Content>
-      </div>
+      <MuiThemeProvider muiTheme={ muiTheme }>
+        <div>
+          <LoginModal
+            onSubmit={ login }
+            isPending={ session.get('isLoading', false) }
+            hasError={ session.get('hasError', false) }
+            open={ !isLoggedIn } />
+
+          <AppBar
+            iconElementLeft={
+              <IconButton
+                onTouchTap={ this.toggleDrawer.bind(this) }>
+                <MenuIcon />
+              </IconButton>
+            }
+            iconElementRight={
+              <IconMenu
+                iconButtonElement={
+                  <IconButton>
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                targetOrigin={ {horizontal: 'right', vertical: 'top'} }
+                anchorOrigin={ {horizontal: 'right', vertical: 'top'} }>
+                <MenuItem onTouchTap={ logout } primaryText="Sign out" />
+              </IconMenu>
+            }
+          />
+          <Drawer
+            open={ drawerOpen }
+            docked={ false }
+            onRequestChange={ this.toggleDrawer.bind(this) }>
+            <List>
+              <ListItem
+                primaryText="Orders"
+                onTouchTap={ this.closeDrawer.bind(this) }
+                containerElement={ <Link to="/" />}
+                leftIcon={ <MenuIcon /> }
+              />
+              <ListItem
+              primaryText="Settings"
+              onTouchTap={ this.closeDrawer.bind(this) }
+              containerElement={ <Link to="/settings" />}
+              leftIcon={<AccessibilityIcon />} />
+            </List>
+          </Drawer>
+          <Content isVisible={ isLoggedIn }>
+            { children }
+          </Content>
+        </div>
+      </MuiThemeProvider>
     );
+  }
+
+  closeDrawer() {
+    this.setState({
+      drawerOpen: false,
+    });
+  }
+
+  toggleDrawer() {
+    this.setState({
+      drawerOpen: !this.state.drawerOpen,
+    });
   }
 }
 
